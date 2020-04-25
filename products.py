8ab669ca3438cdea3b5067343d48d6fa1c97a65e
Neo <a0927024706@yahoo.com.tw>
2020-04-25 13:12:45 +0800

    first commit

---
+++ products.py
@@ -0,0 +1,17 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入價格:')
+	products.append([name, price])
+	"""
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)
+
+	"""
+print (products)
+
+
